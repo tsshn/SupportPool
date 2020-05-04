1e9c83312646019334307408a7c86c3bdb90ef29
tsshn <ilya.yatsyshyn@gmail.com>
2020-05-04 04:01:10 +0300

    Update UserDetails.java

--- src/main/java/com/project/JavaEE/entities/UserDetails.java
+++ src/main/java/com/project/JavaEE/entities/UserDetails.java
@@ -2,12 +2,10 @@ package com.project.JavaEE.entities;
 
 import lombok.Getter;
 import lombok.ToString;
-
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 
 import java.util.List;
-import java.util.Set;
 
 @Getter
 @ToString(callSuper = true)
